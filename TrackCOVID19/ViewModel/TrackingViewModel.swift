e8c9c1fd4cd73267e3d63d3345d83d17ba55015f
tniraj7 <tniraj7@gmail.com>
2020-05-01 13:54:30 +0900

    add TrackingViewModel to provide data to UI

---
+++ TrackCOVID19/ViewModel/TrackingViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TrackingViewModel.swift
+//  TrackCOVID19
+//
+//  Created by Tiwari, Niraj | Nero | LPD on 2020/05/01.
+//  Copyright © 2020 NT. All rights reserved.
+//
+
+import Foundation
