d990aedf10d782a7593ad4f70ceff440ddd0e6f9
tniraj7 <tniraj7@gmail.com>
2020-05-01 15:10:28 +0900

    * Make UI * Bind data to ContentView using Combine

--- TrackCOVID19/View/ContentView.swift
+++ TrackCOVID19/View/ContentView.swift
@@ -1,13 +1,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var trackingListVM = TrackingListViewModel()
+    
+    init() {
+        self.trackingListVM.getTrackingData()
+    }
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            VStack {
+                Text("Information of COVID-19 spread statewise in USA ")
+                Spacer()
+                List(self.trackingListVM.trackings, id: \.id) { tracking in
+                    
+                    HStack{
+                        Text(tracking.state)
+                            .padding()
+                            .foregroundColor(Color.white)
+                            .background(Color.orange)
+                            .clipShape(Circle())
+                            .font(.custom("", size: 22))
+                    }
+                    Spacer()
+                    VStack(alignment: .center) {
+                        
+                        Text("\(tracking.total)")
+                            .foregroundColor(Color.gray)
+                            .font(.custom("", size: 45))
+                        
+                        HStack(alignment: .center, spacing: 10) {
+                            HStack {
+                                Text("☠️")
+                                Text("\(tracking.death)")
+                            }.padding(5)
+                                .foregroundColor(Color.black)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            
+                            HStack {
+                                Text("🏥")
+                                Text("\(tracking.hospitalized)")
+                            }.padding(5)
+                                .foregroundColor(Color.black)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                        }
+                    }
+                     
+                    Spacer()
+                }
+                .navigationBarTitle("COVID-19 Tracking")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.colorScheme, .dark)
     }
 }
