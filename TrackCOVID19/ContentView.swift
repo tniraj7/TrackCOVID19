9aac8fbb2ec6a4459e8e234360453ea22fd8e28f
tniraj7 <tniraj7@gmail.com>
2020-05-01 13:07:23 +0900

    initialize project

--- TrackCOVID19/ContentView.swift
+++ TrackCOVID19/ContentView.swift
@@ -1,11 +1,3 @@
-//
-//  ContentView.swift
-//  TrackCOVID19
-//
-//  Created by Tiwari, Niraj | Nero | LPD on 2020/05/01.
-//  Copyright © 2020 NT. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
