884e6c43dd3db7209d7f2b313b4888208fb9f0ca
tniraj7 <tniraj7@gmail.com>
2020-05-01 13:01:10 +0900

    Initial Commit

---
+++ TrackCOVID19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackCOVID19
+//
+//  Created by Tiwari, Niraj | Nero | LPD on 2020/05/01.
+//  Copyright © 2020 NT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
